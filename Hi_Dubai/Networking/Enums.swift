891883427a4f5220bb1f66054ef77aad2691232b
Asif Khan <asif.khan@hidubai.com>
2023-06-22 09:53:21 +0400

    # Added tmdb_api_key

--- Hi_Dubai/Networking/Enums.swift
+++ Hi_Dubai/Networking/Enums.swift
@@ -11,6 +11,10 @@ enum EndPoint : String {
     case news_updated  = "https://hidubaifocusapi.free.beeceptor.com/articles"
     case x_master_key = "$2b$10$YyUJiWKHl8CtW90XTkp7ru9ysDiWLhw5AsU4UTSSBgV5AUFPyxDfy"
     case pumpkin     = "https://api.punkapi.com/v2/beers"
+    case searchMovie  = "https://api.themoviedb.org/3/search/movie"
+    case popularMovie =  "https://api.themoviedb.org/3/movie/popular"
+    case tmdb_api_key = "020e7b126f0ee278311159ff7dd3028c"
+    case tmdb_access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMjBlN2IxMjZmMGVlMjc4MzExMTU5ZmY3ZGQzMDI4YyIsInN1YiI6IjY0OTJhNTExNGJhNTIyMDEzOTM4NzA2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CzMJ5X8uv94mXBcmqh0bQPIXwDcEUOYBC53GNiS2dcw"
 }
 
 public enum AppNetworkingHttpMethods: String {
